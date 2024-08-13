58b84aa4188c24757c65be8663ea3ff5280d4af1
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-13 16:20:55 +0700

    feat: create database models

---
+++ migrations/20240813091545-create-room.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rooms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      winner: {
+        type: Sequelize.STRING
+      },
+      playerCount: {
+        type: Sequelize.INTEGER
+      },
+      isFinished: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rooms');
+  }
+};
