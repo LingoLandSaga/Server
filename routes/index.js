f315066bf0277ab5bc200cabc31a1efdbb7fdfb8
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-08-14 20:41:30 +0700

    feat: added routes for rooms

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const errorHandler = require("../middlewares/errorHandler")
 router.get("/", HomeController.home);
 router.get('/random-words', HomeController.randomWords);
 router.post('/check-word', HomeController.checkWord);
-router.get("/make-room", HomeController.makeRoom)
+router.get("/rooms", HomeController.getRooms)
+router.post("/rooms", HomeController.makeRoom)
+router.post("/rooms/:roomId/join", HomeController.joinRoom)
 router.use(errorHandler)
 
 module.exports = router
