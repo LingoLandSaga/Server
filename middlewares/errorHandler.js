d4a7991225a5fe97b59c1cedee4e543ff858f553
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-14 10:23:25 +0700

    fix: change errorHandler to synchronous

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,18 @@
-const errorHandler = async (err, req, res, next) => {
-  try {
+const errorHandler = (err, req, res, next) => {
+  let status = 500;
+  let message = 'Internal Server Error';
 
-  } catch (error) {
-
+  switch(err.name) {
+    case 'WordNotFound':
+      status = 404;
+      message = 'Word Not Found';
+      break;
+    case 'AnswerNotValid':
+      status = 400;
+      message = 'You must answer according to the given words';
+      break;
   }
+  res.status(status).json({message});
 }
+
 module.exports = errorHandler
