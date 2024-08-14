54f7d7d265a9595032be260f068521298e56274f
ariiqramadhan <ariiq.ramadhan@gmail.com>
2024-08-14 10:24:52 +0700

    feat: create word randomizer and answer checker

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,48 @@
+const { Op } = require('sequelize');
+const { Word } = require('../models');
+
 class HomeController {
   static async home(req, res, next) {
     try {
       res.send("Welcome")
     } catch (error) {
-      res.send(error)
+      next(error)
+    }
+  }
+  
+  static async randomWords(req, res, next) {
+    try {
+      const randomWord = Math.ceil(Math.random() * 108047);
+      const { id, word } = await Word.findByPk(randomWord);
+      const randomShow = Math.floor(Math.random() * Math.floor(word.length / 2));
+      const shortWord = word.slice(0, randomShow + 1)
+      res.status(200).json({id, word, shortWord});
+    } catch (error) {
+      next(error)
+    }
+  }
+  
+  static async checkWord(req, res, next) {
+    try {
+      let { answer, question } = req.body;
+      answer = answer.toLowerCase();
+      question = question.toLowerCase();
+      if (!answer.includes(question)) throw {name: 'AnswerNotValid'}
+
+      const findWord = await Word.findOne({
+        where: {
+          word: {
+            [Op.iLike] : answer 
+          }
+        },
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      });
+      if (!findWord) throw {name: 'WordNotFound'};
+      res.status(200).json(findWord);
+    } catch (error) {
+      next(error)
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-const HomeController = require("../controllers/HomeController")
-
 const router = require("express").Router()
+const HomeController = require("../controllers/HomeController")
+const errorHandler = require("../middlewares/errorHandler")
 
-router.get("/", HomeController.home)
+router.get("/", HomeController.home);
+router.get('/random-words', HomeController.randomWords);
+router.post('/check-word', HomeController.checkWord);
+
+router.use(errorHandler)
 
 module.exports = router
